95e5f83a6f429c8070a71ec9017a6ac8be53fa5b
AbdulRehman-9876 <abdulrehman9876@gmail.com>
2025-08-15 15:16:46 +0500

    Dialog Added for delete user

--- Frontend/src/Components/User/DeleteUserButton.jsx
+++ Frontend/src/Components/User/DeleteUserButton.jsx
@@ -1,13 +1,54 @@
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-export default function () {
-  const handleDelete = () => {};
+export default function MyComponent() {
+  const [open, setOpen] = useState(false);
+
+  const handleDeleteClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Button onClick={handleDelete} variant="contained"
-         sx={{ ml: 107, mt: 2, mb: -8.5 }}>
+      <Button
+        onClick={handleDeleteClick}
+        variant="contained"
+        sx={{ ml: 107, mt: 2, mb: -8.5 }}
+      >
         Delete User Account
       </Button>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sure, you want to delete yout account?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This action cannot be reveresed and you will lose all your data, including 
+            your todos. You can use this application by registering your account.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} autoFocus>
+            Delete my account
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
